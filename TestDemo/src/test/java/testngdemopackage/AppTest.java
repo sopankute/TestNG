f28e94055040d42600be5cde6f3fa8a99a3111b0
sopan kute <sopankutew@gmail.com>
2024-09-30 17:14:19 +0530

    testNG practice

---
+++ TestDemo/src/test/java/testngdemopackage/AppTest.java
@@ -0,0 +1,12 @@
+package testngdemopackage;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+    @Test
+    public void sample()
+    {
+        System.out.println("AppTest.sample() | TestNG Project");
+    }
+}
